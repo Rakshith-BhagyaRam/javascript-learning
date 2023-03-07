297a1e2fea3b935838b94abe4ff11d8c198f924f
Rakshith-BhagyaRam <126672273+Rakshith-BhagyaRam@users.noreply.github.com>
2023-03-07 15:43:40 +0530

    Create for-ofLoop.js

---
+++ 09-Data-Structures-Operators/starter/for-ofLoop.js
@@ -0,0 +1,23 @@
+
+//// for-of Loop
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// for (const item of menu) {
+//   console.log(item);
+// }
+
+// for (const item of menu.entries()) {
+//   console.log(item);
+// }
+
+// for (const item of menu.entries()) {
+//   console.log(`${item[0] + 1}: ${item[1]} `);
+// }
+
+// console.log("--------------=======---------");
+
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el} `);
+// }
+
+// console.log([...menu.entries()]);
+
