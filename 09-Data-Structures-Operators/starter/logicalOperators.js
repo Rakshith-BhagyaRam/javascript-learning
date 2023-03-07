5e6c24b3b0be18edf7c0d1b8496a973039130fc3
Rakshith-BhagyaRam <126672273+Rakshith-BhagyaRam@users.noreply.github.com>
2023-03-07 15:47:56 +0530

    Create logicalOperators.js

---
+++ 09-Data-Structures-Operators/starter/logicalOperators.js
@@ -0,0 +1,36 @@
+
+//////////////////////// logical operators \\\\\\\\\\\\\\\\\\\\
+
+// OR Assignment operator
+
+console.log('------------------------------------');
+
+// resto1.Guests = resto1.Guests || 10;
+// resto2.Guests = resto2.Guests || 10;
+
+// resto1.Guests ||= 10;
+// resto2.Guests ||= 10;
+
+// Nullish assignment operator (null or undefined )
+
+console.log('------------------------------------');
+
+// resto2.Guests = resto2.Guests ?? 10;
+// resto1.Guests = resto1.Guests ?? 10;
+
+// resto1.Guests ??= 10;
+// resto2.Guests ??= 10;
+
+// AND Assignment operator
+
+// resto1.owner = resto1.owner && '<Anomimos>';
+// resto2.owner = resto2.owner && '<Anomimos>';
+
+resto1.owner &&= '<Anomimos>';
+resto2.owner &&= '<Anomimos>';
+
+console.log(resto1);
+console.log(resto2);
+
+
+*/
